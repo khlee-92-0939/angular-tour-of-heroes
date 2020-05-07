5d8653e46d7bd847add15f4e6ac0f9120da79716
khlee <khlee@kimids.co.kr>
2020-05-07 10:00:24 +0900

    4. 서비스 추가하기

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel은 이 패키지가 제공합니다.
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // `providedIn`을 사용했기 때문에 프로바이더는 등록하지 않습니다.
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
